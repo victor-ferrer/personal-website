f09c02d40ec529527e3f6127f26ee3c7f66eb4c5
victor-ferrer <vferrer_2005@hotmail.com>
2025-01-06 23:08:47 +0100

    chore: Added route 53 record

--- tfe/main.tf
+++ tfe/main.tf
@@ -37,6 +37,57 @@ resource "aws_s3_bucket_policy" "cv_bucket_policy" {
   })
 }
 
+resource "aws_cloudfront_distribution" "cv_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.cv_bucket.website_endpoint
+    origin_id = "S3-CV-Origin"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["SSLv3"]
+    }
+    
+  }
+  enabled = true
+  default_root_object = "index.html"
+  restrictions {
+    geo_restriction {
+      locations = []
+      restriction_type = "none"
+    } 
+  }
+  default_cache_behavior {
+    allowed_methods = ["GET","HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-CV-Origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      cookies {
+        forward = "none"
+      }
+      query_string = true
+    }
+  }
+  
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_route53_zone" "my_zone" {
+  name = "victorferrerposa.com"
+}
+
+resource "aws_route53_record" "cv_record" {
+  zone_id = aws_route53_zone.my_zone.id
+  name = "www"
+  type = "CNAME"
+  ttl = 300
+  records = [aws_cloudfront_distribution.cv_distribution.domain_name]
+}
+
 resource "aws_s3_object" "cv_files" {
   for_each = fileset("${path.module}/../site_files", "**")
   bucket   = aws_s3_bucket.cv_bucket.id
@@ -57,3 +108,7 @@ output "s3_website_url" {
   value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
   description = "S3 static website URL"
 }   
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.cv_distribution.domain_name
+}
